a3e8e38e253d5ee7ac3d9fc605da5927a531e0e0
sebastientinel <sebastien.tinel@gmail.com>
2019-07-09 16:35:01 +0200

    Update create_task_log.py
    Use of CreateTaskLogAction object as defined in models.

--- actions/create_task_log.py
+++ actions/create_task_log.py
@@ -1,4 +1,5 @@
 from thehive4py.api import TheHiveApi
+from thehive4py.models import CaseTaskLog
 from st2common.runners.base_action import Action
 
 __all__ = [
@@ -9,6 +10,7 @@ __all__ = [
 class CreateTaskLogAction(Action):
     def run(self, task_id, log):
         api = TheHiveApi(self.config['thehive_url'], self.config['thehive_api_key'])
-        response = api.create_task_log(task_id, log)
+        case_task_log = CaseTaskLog(message=log)
+        response = api.create_task_log(task_id, case_task_log)
 
         return response.json()
