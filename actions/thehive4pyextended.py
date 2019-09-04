6198e827a4bf6162755bd94729f100e8f4ac941c
Florian GAULTIER <fgaultier@vente-privee.com>
2019-09-04 11:13:08 +0200

    Flake8

--- actions/thehive4pyextended.py
+++ actions/thehive4pyextended.py
@@ -3,6 +3,7 @@ import json
 from thehive4py.api import TheHiveApi
 from thehive4py.exceptions import CaseTaskException, AlertException
 
+
 class TheHiveApiExtended(TheHiveApi):
     def get_task(self, task_id):
         req = self.url + "/api/case/task/{}".format(task_id)
@@ -10,11 +11,13 @@ class TheHiveApiExtended(TheHiveApi):
             return requests.get(req, proxies=self.proxies, auth=self.auth, verify=self.cert)
         except requests.exceptions.RequestException as e:
             raise CaseTaskException("Case task logs search error: {}".format(e))
+
     def get_analyzer_by_name_and_data_type(self, name, data_type):
         req = self.url + "/api/connector/cortex/analyzer/type/{}".format(data_type)
         analyzers = requests.get(req, proxies=self.proxies, auth=self.auth, verify=self.cert)
         analyzer = [a for a in analyzers.json() if a['name'].lower() == name.lower()]
         return analyzer[0]
+
     def promote_alert_to_case(self, alert_id, case_template=None):
         """
             This uses the TheHiveAPI to promote an alert to a case
