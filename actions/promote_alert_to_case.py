dfc86d35267ded0cfb5cea9fb5bd63e54b926106
Florian GAULTIER <fgaultier@vente-privee.com>
2019-09-04 11:08:38 +0200

    TheHiveApiExtended is considered as an action and crash on https://github.com/StackStorm/st2/blob/master/st2common/st2common/util/loader.py#L69

--- actions/promote_alert_to_case.py
+++ actions/promote_alert_to_case.py
@@ -1,32 +1,5 @@
-import requests
-import json
-from thehive4py.api import TheHiveApi
-from thehive4py.exceptions import AlertException
 from st2common.runners.base_action import Action
-
-
-class TheHiveApiExtended(TheHiveApi):
-    def promote_alert_to_case(self, alert_id, case_template=None):
-        """
-            This uses the TheHiveAPI to promote an alert to a case
-            :param alert_id: Alert identifier
-            :param case_template: Optional Case Template name
-            :return: TheHive Case
-            :rtype: json
-        """
-
-        req = self.url + "/api/alert/{}/createCase".format(alert_id)
-
-        try:
-            return requests.post(req, headers={'Content-Type': 'application/json'},
-                                 proxies=self.proxies, auth=self.auth,
-                                 verify=self.cert, data=json.dumps({"caseTemplate": case_template}))
-
-        except requests.exceptions.RequestException as the_exception:
-            raise AlertException("Couldn't promote alert to case: {}".format(the_exception))
-
-        return None
-
+from thehive4pyextended import TheHiveApiExtended
 
 __all__ = [
     'PromoteAlertToCaseAction'
--- actions/run_analyzer_on_data_type.py
+++ actions/run_analyzer_on_data_type.py
@@ -1,17 +1,7 @@
 import random
-import requests
-from thehive4py.api import TheHiveApi
+from thehive4pyextended import TheHiveApiExtended
 from thehive4py.query import Eq
 from st2common.runners.base_action import Action
-
-
-class TheHiveApiExtended(TheHiveApi):
-    def get_analyzer_by_name_and_data_type(self, name, data_type):
-        req = self.url + "/api/connector/cortex/analyzer/type/{}".format(data_type)
-        analyzers = requests.get(req, proxies=self.proxies, auth=self.auth, verify=self.cert)
-        analyzer = [a for a in analyzers.json() if a['name'].lower() == name.lower()]
-        return analyzer[0]
-
 
 __all__ = [
     'RunAnalyzerOnDataTypeAction'
